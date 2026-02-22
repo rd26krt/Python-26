9cdcd72cd0967ebc67df2ba444a7f822a686eb99
rd26krt <dasr84931@gmail.com>
2026-02-22 22:14:26 +0530

    Avg of 2 nos

---
+++ Python_PS2/Average_2nos.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter the first number"))
+num2 = int(input("Enter the second number"))
+
+average =  (num1 + num2) /2
+
+print("Averge of {} and {} is {}".format(num1,num2, average))
