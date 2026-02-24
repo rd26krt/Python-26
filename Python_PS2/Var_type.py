a046b3910df436a1f3baa3cb7a88a706ebc847e0
rd26krt <dasr84931@gmail.com>
2026-02-24 22:24:08 +0530

    Type of variables

---
+++ Python_PS2/Var_type.py
@@ -0,0 +1,11 @@
+a=20
+b=20.12
+c="Rohan"
+d=True
+e='R'
+
+print("Type of variable e is ", type(e))
+print("Type of variable a is ", type(a))
+print("Type of variable b is ", type(b))
+print("Type of variable c is ", type(c))
+print("Type of variable d is ", type(d))
