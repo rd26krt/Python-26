f2b2672a01f026a6861c21b4e791eb2599f235f7
rd26krt <dasr84931@gmail.com>
2026-03-01 17:05:15 +0530

    Names

---
+++ Python_PS5/Names.py
@@ -0,0 +1,12 @@
+Name = {"Rohan" : "Hindi",
+        "Subham": "Maithli",
+        "John": "English",
+        "Rohan": "Japanese"}
+
+print(Name)
+
+Name2 = {"Rohan" : "Hindi",
+        "Subham": "Maithli",
+        "John": "English",
+        "Rahul": "Hindi"}
+print(Name2)
