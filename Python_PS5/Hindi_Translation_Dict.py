ff7ef4889d2f798108e994c93db05da0b63563c7
rd26krt <dasr84931@gmail.com>
2026-03-09 21:29:03 +0530

    Hindi Translate

---
+++ Python_PS5/Hindi_Translation_Dict.py
@@ -0,0 +1,7 @@
+Dict1 = {"नमस्ते" : "Hello/Greetings",
+         "धन्यवाद": "Thank You",
+         "कृपया": "Please",
+         "माफ़ करें": "Sorry/Excuse me"}
+
+print(Dict1)
+print(Dict1["धन्यवाद"])
