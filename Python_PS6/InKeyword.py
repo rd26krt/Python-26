3f848aeaf70be177326c9fc0d45c47b4a059c633
rd26krt <dasr84931@gmail.com>
2026-03-10 21:03:55 +0530

    InKeyword in python

---
+++ Python_PS6/InKeyword.py
@@ -0,0 +1,6 @@
+post = "Rohan is a good and boy and rohan is intelligent".upper()
+
+if("ROHAN" in post):
+    print("The post is talking about Rohan")
+else:
+    print("The post is not talking about Rohan")
