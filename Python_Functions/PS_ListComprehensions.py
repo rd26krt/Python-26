39135b424b2ebf963ab0ff8ac4469c0df3d48f45
rd26krt <dasr84931@gmail.com>
2026-04-22 21:49:01 +0530

    List comprehension

---
+++ Python_Functions/PS_ListComprehensions.py
@@ -0,0 +1,12 @@
+# list1 = []
+# def even(list):
+#     for i in list:
+#         if i%2 == 0:
+#             list1.append(i)
+#          return list1
+
+list = [1,2,3,4,5,6,7,8]
+
+
+list 1 = [i*i for i in list: if i%2==0]
+print(list1)
