8216ed949619ff2ce874bef4105f4c1521ed27c3
rd26krt <dasr84931@gmail.com>
2026-04-28 22:05:45 +0530

    custom exception

---
+++ Python_Classes/PS4_CustomExceptionHandling.py
@@ -0,0 +1,15 @@
+class Error(Exception):
+    pass
+class dobException(Error):
+    pass
+
+year = int(input("Enter the Year of Birth :"))
+age = 2026 - year
+
+try:
+    if(age<=30 and age>20):
+        print("You are eligible for this job")
+    else:
+        raise dobException
+except dobException:
+    print("Try again for a different exam")
