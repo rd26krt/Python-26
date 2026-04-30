22c244aeb3b02063fc1579b129966f1ebefc41cd
rd26krt <dasr84931@gmail.com>
2026-04-30 21:44:27 +0530

    Magic methods

---
+++ Python_Classes/PS6_Magic_or_Dunder_Methods.py
@@ -0,0 +1,23 @@
+class Car:
+    def __new__(cls, doors, windows, engineType):
+        print("The object has started getting created")
+        return super().__new__(cls)
+        
+    def __init__(self, doors, windows, engineType):
+        self.doors = doors
+        self.windows = windows
+        self.engineType = engineType
+    
+    
+    def __str__(self):
+        return "The object has been initialized"
+    
+    def __sizeof__(self):
+        print("It prints the size of the object")
+
+    def drive(self):
+        print("The person drives the car")
+
+C = Car(4,4,"Diesel")
+print(C)
+C.__sizeof__()
