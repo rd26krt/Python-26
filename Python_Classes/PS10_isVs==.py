5bb9401ca5e4627afa63cd8056fc0897d714f0a6
rd26krt <dasr84931@gmail.com>
2026-05-04 21:41:10 +0530

    is vs ==

---
+++ Python_Classes/PS10_isVs==.py
@@ -0,0 +1,24 @@
+list1  = ["Rishikesh", "Somanth", "Ujjain", "Kashi"]
+list2  = ["Rishikesh", "Somanth", "Ujjain", "Kashi"]
+
+print(list1 == list2)
+
+list3  = ["Rishikesh", "Somanth", "Ujjain", "Kashi"]
+list4  = ["Rishikesh", "Somanth", "Omkreshwar", "Kashi"]
+
+print(list3 == list4)
+
+list1  = ["Rishikesh", "Somanth", "Ujjain", "Kashi"]
+list2  = ["Rishikesh", "Somanth", "Ujjain", "Kashi"]
+
+print(list1 is list2)
+
+list7  = ["Rishikesh", "Somanth", "Ujjain", "Kashi"]
+list8  = list7
+
+print(list7 is list8)
+
+list8[0] = "Uttarakhand"
+print(list7)
+print(list8)
+
