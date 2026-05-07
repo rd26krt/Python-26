93855f5802573aaa93229261b2eb38972d8c6694
rd26krt <dasr84931@gmail.com>
2026-05-07 21:37:01 +0530

    Static methods

---
+++ Python_Classes/PS13_StaticMethods.py
@@ -0,0 +1,35 @@
+import datetime
+
+now = datetime.datetime.now()
+class Car:
+    basePrice = 100000
+    def __init__(self, doors, windows, power):
+        self.doors = doors
+        self.windows = windows
+        self.power = power
+        
+    
+    def print_BasePrice(self):
+        print ("The BasePrice is {}".format(self.basePrice))
+        
+    
+    @classmethod
+    def revise_BasePrice(cls, increment):
+        cls.basePrice = cls.basePrice + (cls.basePrice * increment)/100
+     
+    @staticmethod   
+    def checkYear():
+        
+        if now.year == 2025:
+            return True
+        else:
+            return False
+        
+        
+car1 = Car(4,4,2000)
+print(car1.basePrice)
+
+if(car1.checkYear()):
+    pass
+else:
+    car1.revise_BasePrice(10)
