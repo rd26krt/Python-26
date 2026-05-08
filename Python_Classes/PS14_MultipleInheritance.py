dc2f83d16c4ee743e0a447a31106bcb2cef8b711
rd26krt <dasr84931@gmail.com>
2026-05-08 17:49:07 +0530

    multiple Inheritance

---
+++ Python_Classes/PS14_MultipleInheritance.py
@@ -0,0 +1,26 @@
+class A:
+    def method1(self):
+        print("A class method1 called")
+        
+class B(A):
+    def method1(self):
+        print("B class method1 called")
+        super().method1()
+        
+class C(A):
+    def method1(self):
+        print("C class method1 called")
+        super().method1()
+        
+class D(B,C):
+    def method1(self):
+        print("D class method1 called")
+        super().method1()
+        
+d = D()
+d.method1()
+
+#multiple Inheritance
+# B.method1(d)
+# C.method1(d)
+# A.method1(d)
